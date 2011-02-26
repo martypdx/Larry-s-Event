4f50b4f79af7cb7f659915840aa767d2cf7852c6
Marty Nelson <marty.nelson@yahoo.com>
2011-02-26 15:05:56 -0800

    Data driven templates

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ app = express.createServer();
 app.set('views', __dirname + '/views/');
 
 app.get('/guests', function (request, response) {
-  Render(response, 'guests');
+  Render(response, 'guests', 
+    {guests: ['Bob', 'Sally', 'Tim', 'Joe']});
 });
 
 app.post('/guests', function (request, response) {
-  Render(response, 'confirmation');
+  Render(response, 'confirmation', {guest: 'Bob'});
 });
 
-function Render(response, template) {
+function Render(response, template, locals) {
 	var options = { layout: false };
+	if(locals) { options.locals = locals; }
 	response.render(template + '.jade', options);
 }
 
